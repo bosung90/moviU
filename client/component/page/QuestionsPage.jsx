6157368d99750209ca125bb99c130cdd017e2479
Aditi Chakravarthi <aditi@one45.com>
2016-02-28 00:48:54 -0800

    Added the 'My Questions' page. Also added Moment.js library for easy date time formatting.

--- client/component/page/QuestionsPage.jsx
+++ client/component/page/QuestionsPage.jsx
@@ -1,8 +1,68 @@
 QuestionsPage = React.createClass({
+  mixins: [ReactMeteorData],
+  getMeteorData() {
+    return {
+      questions: Questions.find({}).fetch()
+    }
+  },
+  _renderQuestionsTableRows() {
+  	let count = 1
+
+
+  	return this.data.questions.map((question)=>{
+  		var formattedDate = moment(question.createdDate).format("MMM Do, YYYY")
+  		var name = ""
+
+  		if (question.status === 'Unanswered')
+  		{
+  				name = "warning"		
+
+  		} 
+  		else 
+  		{
+  			name = "success"				
+		}
+
+  		return  <tr className = {name}>
+					      	<td>{count++}</td>
+					        <td>
+					        { 
+					        	question.status === 'Unanswered' ?
+					        	question.title
+					        	:
+					        	<a href = '/WorkSession'>{question.title}</a>
+					        }
+					        </td>
+					        <td>{formattedDate} </td>
+					        <td>{question.status}</td>
+					      </tr>	
+  	})
+  },
 	render() {
 		return (
 			<div>
-				Register Page
+				<NavBarLoggedIn />
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+
+				<div className="table-responsive table-bordered table-striped">
+					<table className="table">
+					    <thead>
+					      <tr>
+					      	<th>#</th>
+					        <th>My Questions</th>
+					        <th>Date</th>
+					        <th>Status</th>
+					      </tr>
+					    </thead>
+					    <tbody>
+					    	{this._renderQuestionsTableRows()}
+
+					    </tbody>
+					  </table>
+				  </div>
 			</div>
 		)
 	}
