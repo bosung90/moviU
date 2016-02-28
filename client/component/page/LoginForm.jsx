6fa1842243bf377a128ca08895f2f09b4653f0e0
kayla8l <kaylalee@gmail.com>
2016-02-28 13:01:42 -0800

    jk

--- client/component/page/LoginForm.jsx
+++ client/component/page/LoginForm.jsx
@@ -23,33 +23,39 @@ LoginForm = React.createClass({
 		} else {
 			return (
 				<div>
+					<div style={{position: "relative", width: 50 + '%'}}>
+					<div style={{position: "absolute", top: 50 + '%', left: 50 + '%', backgroundColor: 'white', color: 'black', border: '1px solid black', padding: '20px'}}>
 					<form key='0' id="movi_login_form" className="span_12" onSubmit={this.handleLogin}>
 						<h3>Welcome to {this.props.courseId}.</h3>
-						<h3>Create or login to start earning bonus marks.</h3>
+						<h4><i>Register or login to start earning bonus marks.</i></h4>
 						<br />
 						<table className="movi_login_table no-spacing span_12" style={{marginTop: 10 + 'px'}}>
 							<tbody>
 							<tr>
 								<td>
 									<label htmlFor="email">E-mail: </label>
-									<input id="email" name="email" type="email" ref='loginEmailInput' required/>
+									<input id="email" style={{paddingLeft: '20px', width: 400}} placeholder="example@example.com" name="email" type="email" ref='loginEmailInput' required/>
 								</td>
 							</tr>
 							<tr>
 								<td>
 									<label htmlFor="password">Password: </label>
-									<input id="password" name="password" type="password" ref='loginPasswordInput' required/>
+									<input id="password" style={{paddingLeft: '20px', width: 400}} placeholder="password" name="password" type="password" ref='loginPasswordInput' required/>
 								</td>
 							</tr>
 							</tbody>
 						</table>
-						<button className="btn btn-primary" style={{marginTop: 9 + 'px', height: 51 + 'px'}} type="submit">
+						<br></br>
+						<button className="btn btn-default btn-lg btn-block" style={{marginTop: 9 + 'px', height: 51 + 'px'}} type="submit">
 							Login
 						</button>
-					</form>
-					<button className="btn btn-primary" style={{marginTop: 9 + 'px', height: 51 + 'px'}} onClick={()=>FlowRouter.go('/RegisterPage/' + this.props.courseId)}>
-						Create Account
+						<br></br>
+						<button className="btn btn-default btn-lg btn-block" style={{marginTop: 9 + 'px', height: 51 + 'px'}} onClick={()=>FlowRouter.go('/RegisterPage/' + this.props.courseId)}>
+						Register for an Account
 					</button>
+					</form>
+					</div>
+					</div>
 				</div>
 			)
 		}
--- client/component/page/RegisterForm.jsx
+++ client/component/page/RegisterForm.jsx
@@ -6,6 +6,7 @@ RegisterForm = React.createClass({
     }
   },
 	handleRegister(event) {
+		// alert('akkkkksdfdf')
 		event.preventDefault()
 		const name = this.refs.registerNameInput.value.trim()
 		const email = this.refs.registerEmailInput.value.trim()
@@ -32,51 +33,60 @@ RegisterForm = React.createClass({
 	render() {
 		return (
 			<div>
-				<div id="moviU_register"><center>
+				<div id="moviU_register">
 					<div className="moviU_container">
-						<h1 style = {{marginRight: "15%"}}>Register for {this.props.courseId}</h1>
-						<br />
-						<form id="movi_register_form" className="col-sm-12" onSubmit={this.handleRegister}>
+					<div style={{position: "relative", width: 50 + '%'}}>
+					<div style={{position: "absolute", top: 50 + '%', left: 50 + '%', color: 'black', backgroundColor: 'white', border: '1px solid black', padding: '20px'}}>
+						<form id="movi_register_form" style={{color: 'black', width: '500px'}} className="col-sm-12" onSubmit={this.handleRegister}>
+							<label htmlFor="courseNum" style={{marginRight: 20 + 'px', position: "absolute", fontSize: 25 + 'px'}}>Register for: {this.props.courseId} </label>
+							<br></br>
+							<br></br>
+							<br></br>
 							<div className="form-group">
-								<div className="no-spacing col-sm-12 movi_register_form" align = "centre"> <table align="right">
+								<div className="no-spacing col-sm-12 movi_register_form" align = "centre"> 
 									<div className="col-sm-10">
 										<label htmlFor="name"> </label>
-										<input type="name" name="name" id="name" placeholder="Your Name" className="span_12" ref='registerNameInput' required/>
+										<input type="name" style={{paddingLeft: '20px', width: 400}} name="name" id="name" placeholder="Your Name" className="span_12" ref='registerNameInput' required/>
 										<br/>
 									</div>
+									<br></br>
 									<div className="col-sm-10">
 										<label htmlFor="email"> </label>
-										<input type="email" name="email" id="email" placeholder="Your email" className="span_12" ref='registerEmailInput' required/>
+										<input type="email" style={{paddingLeft: '20px', width: 400}} name="email" id="email" placeholder="Your email" className="span_12" ref='registerEmailInput' required/>
 										<br/>
 									</div>
+									<br></br>
 									<div className="col-sm-10">
 										<label htmlFor="password"> </label>
-										<input type="password" name="password" id="password" placeholder="Your password" className="span_12" ref='registerPasswordInput' required/>
+										<input type="password" style={{paddingLeft: '20px', width: 400}} name="password" id="password" placeholder="Your password" className="span_12" ref='registerPasswordInput' required/>
 										<br/>
 									</div>
+									<br></br>
 									<div className="col-sm-10">
 										<label htmlFor="confirm_password"></label>
-										<input  type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password"  className="span_12" ref='registerConfirmPasswordInput' required/>
+										<input  type="password" style={{paddingLeft: '20px', width: 400}} name="confirm_password" id="confirm_password" placeholder="Confirm Password"  className="span_12" ref='registerConfirmPasswordInput' required/>
 										<br/>
 									</div>
-									</table>
-
 								</div>
 								<div className="col-sm-10">
-
 								<div className="checkbox_agreement">
-									<p style={{marginTop: 10}}> <input type="checkbox"  ref='checkbox_agreement' required/> I agree with the <a href="">terms and conditions. </a></p> 
-
+									<p style={{marginTop: 10, color: 'black'}}> <input type="checkbox"  ref='checkbox_agreement' required/>I agree with the <a href="">terms and conditions. </a></p> 
 								</div>
 								</div>
 							</div>
-							<button style={{marginTop: 9, height: 40, marginRight: "16%", width: "17%", fontSize: '100%'}} className="btn btn-primary" type="submit" >
+							<button style={{marginTop: 9, height: 40, marginRight: "16%", width: "100%", fontSize: '100%'}} className="btn btn-default btn-lg btn-block" type="submit" >
 								Sign Up
 							</button>
-							<div style = {{marginRight: "15%"}}> <center><p>Already a member?<a href={'/Course/' + this.props.courseId }> Login</a></p> </center></div>
+							<br></br>
+							<br></br>
+							<br></br>
+							<label htmlFor="courseNum" style={{marginRight: 20 + 'px', position: "absolute", fontSize: 20 + 'px'}}>Already a member?</label>
+							<br></br>
+							<button style = {{marginTop: 9, height: 40, marginRight: "16%", width: "100%"}} className="btn btn-default btn-lg btn-block"><a href={'/Course/' + this.props.courseId }>Login</a></button>
 						</form>
+						</div>
+						</div>
 					</div>
-					</center>
 				</div>
 			</div>
 		)
