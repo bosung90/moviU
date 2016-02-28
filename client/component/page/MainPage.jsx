2acfff815c9dbc6137aee54ead0513d0813dd5ef
Eric Kim <bosung90@gmail.com>
2016-02-28 02:20:11 -0800

    fixed warning in registerform mainpage refactor

--- client/component/page/MainPage.jsx
+++ client/component/page/MainPage.jsx
@@ -2,14 +2,8 @@ MainPage = React.createClass({
 	render() {
 		return (
 			<div>
-				<div>
-					<NavBar />
-					<div>
-						<div style={{color: 'purple', paddingLeft: 50}}></div>
-						<RegisterForm />
-					</div>
-				</div>
-				<p>This is the main page!</p>
+				<NavBar />
+				<RegisterForm />
 			</div>
 		)
 	}
--- client/component/page/RegisterForm.jsx
+++ client/component/page/RegisterForm.jsx
@@ -45,50 +45,37 @@ RegisterForm = React.createClass({
 					<div className="moviU_container">
 						<h1>Register for an Account!</h1>
 						<br />
-						<form id="movi_register_form" className="span_12" onSubmit={this.handleRegister}>
+						<form id="movi_register_form" className="col-sm-12" onSubmit={this.handleRegister}>
 							<div className="form-group">
-								<table className="no-spacing span_12 movi_register_form">
-									<tbody>
-										<tr>
-											<div className="col-sm-10">
-												<label htmlFor="name">Name:  </label>
-												<br/>
-												<input type="name" name="name" id="name" placeholder="Name" className="span_12" ref='registerNameInput' required/>
-											</div>
-											<br/>
-										</tr>
-										<tr>
-											<div className="col-sm-10">
-												<label htmlFor="email">E-mail:  </label>
-												<br/>
-												<input type="email" name="email" id="email" placeholder="E-mail" className="span_12" ref='registerEmailInput' required/>
-											</div>
-											<br/>
-										</tr>
-										<tr>
-											<div className="col-sm-10">
-												<label htmlFor="password">Password:  </label>
-												<br/>
-												<input type="password" name="password" id="password" placeholder="Password" className="span_12" ref='registerPasswordInput' required/>
-											</div>
-											<br/>
-										</tr>
-										<tr>
-											<div className="col-sm-10">
-												<label htmlFor="confirm_password">Confirm Password:  </label>
-												<br/>
-												<input type="password" name="confirm_password" id="confirm_password" placeholder="Password" className="span_12" ref='registerConfirmPasswordInput' required/>
-											</div>
-											<br/>
-										</tr>
-									</tbody>
-								</table>
+								<div className="no-spacing col-sm-12 movi_register_form">
+									<div className="col-sm-10">
+										<label htmlFor="name">Name:  </label>
+										<input type="name" name="name" id="name" placeholder="Name" className="span_12" ref='registerNameInput' required/>
+										<br/>
+									</div>
+									<div className="col-sm-10">
+										<label htmlFor="email">E-mail:  </label>
+										<input type="email" name="email" id="email" placeholder="E-mail" className="span_12" ref='registerEmailInput' required/>
+										<br/>
+									</div>
+									<div className="col-sm-10">
+										<label htmlFor="password">Password:  </label>
+										<input type="password" name="password" id="password" placeholder="Password" className="span_12" ref='registerPasswordInput' required/>
+										<br/>
+									</div>
+									<div className="col-sm-10">
+										<label htmlFor="confirm_password">Confirm Password:  </label>
+										<input type="password" name="confirm_password" id="confirm_password" placeholder="Password" className="span_12" ref='registerConfirmPasswordInput' required/>
+										<br/>
+									</div>
+								</div>
 								<div className="checkbox_agreement">
-									<input type="checkbox" style={{marginTop: 10, float: 'left'}} ref='checkbox_agreement' required/><p style={{marginTop: 10}}> I agree to the terms and conditions. This is a disclaimer test. This helps us not get sued and allows us to operate safely and easily.</p> 
+									<input type="checkbox" style={{float: 'left', marginRight: '4px'}} ref='checkbox_agreement' required/>
+									<p style={{marginTop: 10}}> I agree to the terms and conditions. This is a disclaimer test. This helps us not get sued and allows us to operate safely and easily.</p> 
 								</div>
 							</div>
-							<button style={{marginTop: 9, height: 68, fontSize: '100%'}} className="btn btn-primary" type="submit" >
-									Register
+							<button style={{marginTop: 9, height: 44, fontSize: '100%'}} className="btn btn-primary" type="submit" >
+								Register
 							</button>
 						</form>
 					</div>
