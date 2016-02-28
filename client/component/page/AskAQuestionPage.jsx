0d553b19570386e74f9a2c35617a997c01f84a9e
Kelvin Kou <k.kou@alumni.ubc.ca>
2016-02-28 12:16:56 -0800

    made askquestion page prettier; centered some tables

--- client/component/page/AskAQuestionPage.jsx
+++ client/component/page/AskAQuestionPage.jsx
@@ -24,16 +24,14 @@ AskAQuestionPage = React.createClass({
 				<form onSubmit={this._onQuestionSubmit}>
 					<div style={{ width: "60%" }} className="center-block">
 						<h2>Ask away!</h2>
-						<div className="input-group">
-							<span className="input-group-addon" id="question-title">Title</span>
-							<input type="text" className="form-control" placeholder="Your Question Title" aria-describedby="question-title" />
+						<div className="form-group">
+                                                    <input id="question-title" type="text" className="form-control" placeholder="Your Question Title" />
 						</div>
-						<div className="input-group">
-							<span className="input-group-addon" id="question-description">Description</span>
-							<textarea style={{ height: "200px", minHeight: "200px", maxHeight: "200px" }} type="text" className="form-control" placeholder="Your Question Description" aria-describedby="question-description" />
+						<div className="form-group">
+						    <textarea style={{ height: "200px", minHeight: "200px", maxHeight: "200px" }} id="question-description" type="text" className="form-control" placeholder="Your Question Description" />
 						</div>
-						<div className="clearfix">
-							<button type="submit" className="btn btn-default pull-right">Submit</button>
+						<div className="center-block">
+							<button type="submit" className="btn btn-default">Ask a classmate!</button>
 						</div>
 					</div>
 				</form>
---
+++ client/component/page/ChatBox.jsx
@@ -0,0 +1,91 @@
+ChatBox = React.createClass({
+    render() {
+        return (
+<div className="container">
+    <div className="row">
+        <div className="col-md-5">
+            <div className="panel panel-primary">
+                <div className="panel-heading">
+                    <span className="glyphicon glyphicon-comment"></span> Chat
+                </div>
+                <div className="panel-body">
+                    <ul className="chat">
+                        <li className="left clearfix"><span className="chat-img pull-left">
+                            <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" className="img-circle" />
+                        </span>
+                            <div className="chat-body clearfix">
+                                <div className="header">
+                                    <strong className="primary-font">Jack Sparrow</strong> <small className="pull-right text-muted">
+                                        <span className="glyphicon glyphicon-time"></span>12 mins ago</small>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                    dolor, quis ullamcorper ligula sodales.
+                                </p>
+                            </div>
+                        </li>
+                        <li className="right clearfix"><span className="chat-img pull-right">
+                            <img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="User Avatar" className="img-circle" />
+                        </span>
+                            <div className="chat-body clearfix">
+                                <div className="header">
+                                    <small className=" text-muted"><span className="glyphicon glyphicon-time"></span>13 mins ago</small>
+                                    <strong className="pull-right primary-font">Bhaumik Patel</strong>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                    dolor, quis ullamcorper ligula sodales.
+                                </p>
+                            </div>
+                        </li>
+                        <li className="left clearfix"><span className="chat-img pull-left">
+                            <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" className="img-circle" />
+                        </span>
+                            <div className="chat-body clearfix">
+                                <div className="header">
+                                    <strong className="primary-font">Jack Sparrow</strong> <small className="pull-right text-muted">
+                                        <span className="glyphicon glyphicon-time"></span>14 mins ago</small>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                    dolor, quis ullamcorper ligula sodales.
+                                </p>
+                            </div>
+                        </li>
+                        <li className="right clearfix"><span className="chat-img pull-right">
+                            <img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="User Avatar" className="img-circle" />
+                        </span>
+                            <div className="chat-body clearfix">
+                                <div className="header">
+                                    <small className=" text-muted"><span className="glyphicon glyphicon-time"></span>15 mins ago</small>
+                                    <strong className="pull-right primary-font">Bhaumik Patel</strong>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                    dolor, quis ullamcorper ligula sodales.
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="panel-footer">
+                    <div className="input-group">
+                        <input id="btn-input" type="text" className="form-control input-sm" placeholder="Type your message here..." />
+                        <form onSubmit={this.props.onChatSubmit}>
+                        <span className="input-group-btn">
+                            <button className="btn btn-warning btn-sm" id="btn-chat">
+                                Send</button>
+                        </span>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+            )
+    }
+})
